e1700ff81f8d50aeb91f2ac8a734d2a908aa31d4
Andrei Reyes <rchristianandrei@gmail.com>
2024-11-28 21:29:13 +0800

    create login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './features/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
